6c69469328550c87a778a7e585736fd2089f756a
aszx101123-debug <aszx101123@gmail.com>
2026-09-20 22:41:37 +0900

    chore: refresh v1.2.6 service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = 'webcraft-v1.2.5-water-ore-wheel-detail-20260920';
+const VERSION = 'webcraft-v1.2.6-underwater-inventory-thirdperson-20260920';
 const CORE = [
   './',
   './index.html',
