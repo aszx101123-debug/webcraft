e3caac6a92355b0c18347daeb6d501acec2b2462
aszx101123-debug <aszx101123@gmail.com>
2026-09-21 15:51:43 +0900

    v1.2.7: cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = 'webcraft-v1.2.6.3-texture-startup-20260921';
+const VERSION = 'webcraft-v1.2.7-arms-crafting-armor-biomes-20260921';
 const CORE = [
   './',
   './index.html',
